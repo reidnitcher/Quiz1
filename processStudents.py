23af67a8416fb1e8c95734fb8a387f7f6b5ead75
Reid Nitcher <reid_nitcher1@baylor.edu>
2022-02-14 15:51:11 -0600

    ren

--- processStudents.py
+++ processStudents.py
@@ -21,33 +21,46 @@ through the logic of the problem.
 
 
 import csv
-
+from xml.etree.ElementPath import get_parent_map
+from matplotlib.cbook import open_file_cm
 
 # create a file object to open the file in read mode
-
+students = open('students.csv', 'r')
 
 
 # create a csv object from the file object
-
+students_file = csv.reader(students, delimiter = ',')
 
 #skip the header row
-
+next(students_file)
 
 #create an outfile object for the pocessed record
-
+outfile = open('processedStudents.csv', 'w' )
 
 
 #create a new dictionary named 'student_dict'
-
-
+student_dict ={}
 
 #use a loop to iterate through each row of the file
 
+for record in students_file: 
+    student_dict["student id"] = record[0]
+    student_dict["Pin"] = record[1]
+    student_dict["First Name"] = record[2]
+    student_dict["last Name"] = record[3]
+    student_dict["city"] = record[4]
+    student_dict["state"] = record[5]
+    student_dict["Major"] = record[6]
+    student_dict["Classifcation"] = record[7]
+    student_dict["gpa"] = record[8]
+
+
+print(student_dict)
+
     #check if the GPA is below 3.0. If so, write the record to the outfile
-    
+if student_dict['gpa']:
+    outfile.write()
         
-
-
 
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
@@ -56,19 +69,18 @@ import csv
 
 
 
-
 #print the entire dictionary
-
+print(student_dict)
 
 #Print the student id 
-
+print(student_dict['gpa'])
 
 #print out the corresponding GPA from the dictionary
-
+print(student_dict["student id"]["gpa"])
 
 
 #close the outfile
-
+outfile.close()
 
 
 
